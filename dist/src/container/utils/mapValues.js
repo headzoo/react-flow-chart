d2c4013a94bdb12efeaa7fb5e6f4eaf811797d46
headzoo <sean@headzoo.io>
2020-04-26 11:03:40 -0400

    Adds dist files

---
+++ dist/src/container/utils/mapValues.js
@@ -0,0 +1,14 @@
+"use strict";
+// @ts-nocheck
+Object.defineProperty(exports, "__esModule", { value: true });
+function mapValues(o, func) {
+    var res = {};
+    for (var key in o) {
+        if (o.hasOwnProperty(key)) {
+            res[key] = func(o[key]);
+        }
+    }
+    return res;
+}
+exports.default = mapValues;
+//# sourceMappingURL=mapValues.js.map
